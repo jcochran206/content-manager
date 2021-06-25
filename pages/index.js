28d31ab6def0f359cecd2642e8af68928b041daf
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2021-06-24 22:32:21 -0700

    added config file

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from '../components/Navbar';
-import ResourceHighlight from '../components/ResourceHighlight';
-import Newsletter from '../components/Newsletter';
-import Resourcelist from '../components/Resourcelist';
-import Footer from '../components/Footer';
+import Navbar from 'components/Navbar';
+import ResourceHighlight from 'components/ResourceHighlight';
+import Newsletter from 'components/Newsletter';
+import Resourcelist from 'components/Resourcelist';
+import Footer from 'components/Footer';
 
 
 export default function Home() {

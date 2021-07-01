a5fd3ee90407eb122d6495bc391f01ab24c97307
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2021-06-30 21:59:27 -0700

    added fetch call to localhost

---
+++ pages/api/resources.js
@@ -0,0 +1,5 @@
+import data from 'pages/api/data.json';
+
+export default function(req,res){
+    res.send(data)
+} 
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,11 @@ import ResourceHighlight from 'components/ResourceHighlight';
 import Newsletter from 'components/Newsletter';
 import Resourcelist from 'components/Resourcelist';
 import Footer from 'components/Footer';
+import resources from './api/resources';
 
-import {resources} from '../api/data';
 
-export default function Home() {
+
+function Home({resources}) {
   return (
     <Layout>
       <ResourceHighlight 
@@ -21,3 +22,16 @@ export default function Home() {
     </Layout>
   )
 }
+
+export async function getStaticProps() {
+  const resData = await fetch("http://localhost:3000/api/resources");
+  const data = await resData.json();
+
+  return{
+    props: {
+        resources: data
+    }
+  }
+}
+
+export default Home;
